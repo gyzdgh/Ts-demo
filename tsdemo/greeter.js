7e83953b2e8bd7786487cfe8ea29b7cb235ecd57
高远 <gyz@gaozhandeMacBook-Pro.local>
2018-08-14 10:45:39 +0800

    添加一个命令行程序

--- tsdemo/greeter.js
+++ tsdemo/greeter.js
@@ -1,13 +1,13 @@
-var Student = /** @class */ (function () {
-    function Student(姓, 名) {
-        this.姓 = 姓;
-        this.名 = 名;
-        this.姓名 = 姓 + 名;
-    }
-    return Student;
-}());
-function greeter(person) {
-    return "Hello, " + person.姓 + person.名;
-}
-var 全名 = new Student("高", "远");
-console.log(greeter(全名));
+// var Student = /** @class */ (function () {
+//     function Student(姓, 名) {
+//         this.姓 = 姓;
+//         this.名 = 名;
+//         this.姓名 = 姓 + 名;
+//     }
+//     return Student;
+// }());
+// function greeter(person) {
+//     return "Hello, " + person.姓 + person.名;
+// }
+// var 全名 = new Student("高", "远");
+// console.log(greeter(全名));
--- tsdemo/greeter.ts
+++ tsdemo/greeter.ts
@@ -1,19 +1,22 @@
-class Student {
-    姓名: string;
-    constructor(public 姓,public 名) {
-        this.姓名 = 姓 + 名;
-    }
-}
 
-interface Person {
-    姓: string;
-    名: string;
-}
 
-function greeter(person : Person) {
-    return "Hello, " + person.姓 + person.名;
-}
+// class Student {
+//     姓名: string;
+//     constructor(public 姓,public 名) {
+//         this.姓名 = 姓 + 名;
+//     }
+// }
 
-let 全名 = new Student("高","远");
+// interface Person {
+//     姓: string;
+//     名: string;
+// }
 
-console.log(greeter(全名))
+// function greeter(person : Person) {
+//     return "Hello, " + person.姓 + person.名;
+// }
+
+// let 全名 = new Student("高","远");
+
+// console.log(greeter(全名))
+
