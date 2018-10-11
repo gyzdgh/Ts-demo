e40b731d9c4ba4e92eaab181e8465b6db907125e
高远 <gyz@gaozhandeMacBook-Pro.local>
2018-10-11 16:27:23 +0800

    重构 Calculator

--- Typescript-demo-calculator/calculator.ts
+++ Typescript-demo-calculator/calculator.ts
@@ -1,29 +1,111 @@
+
 {
-    function createButton(text: string, container: HTMLElement) {
-        let button: HTMLButtonElement = document.createElement('button');
-        button.textContent = text;
-        document.body.appendChild(button);
+    class Calculator{
+        public n1: string = '';
+        public n2: string = ''
+        public operator: string = ''
+        public result: string = ''
+        public span: HTMLSpanElement
+        public container: HTMLDivElement
+        public textLists: Array<Array<string | number>> = [
+            ['Clear','➗'],
+            ['7','8','9','✖️'],
+            ['4','5','6','-'],
+            ['1','2','3','+'],
+            ['0','.','='],
+        ];
+        constructor(){
+            this.createContainer()
+            this.createElements()
+            this.createButtons()
+            this.bindEvents()
+        }
+        createContainer(){
+            let calc = document.createElement('div')
+            calc.classList.add('calc')
+            document.body.appendChild(calc)
+            this.container = calc
+        }
+        createElements(){
+            let output = document.createElement('div')
+            output.classList.add('output')
+            let span = document.createElement('span')
+            output.appendChild(span)
+            span.textContent = '0'
+            this.span = span
+            this.container.appendChild(output)
+        }
+        createButtons(){
+            this.textLists.forEach((textLists: Array<number | string>)=>{
+                let div = document.createElement('div')
+                div.classList.add('row')
+                textLists.forEach((text: number | string)=>{
+                    this.createButton(text, div, `text-${text}`)
+                })
+                this.container.appendChild(div)
+            })
+        }
+        bindEvents(){
+            this.container.addEventListener('click',event=>{
+                if(event.target instanceof HTMLButtonElement){
+                    const text = event.target.textContent
+                    if('0123456789.'.indexOf(text) >= 0){
+                        if(this.operator){
+                            this.n2 += text
+                            this.span.textContent = this.n2
+                        }else{
+                            this.result = ''
+                            this.n1 += text
+                            this.span.textContent = this.n1
+                        }
+                    }else if('+-✖️➗'.indexOf(text) >= 0){
+                        if(this.result){
+                            this.n1 = this.result
+                            this.result = ''
+                        }
+                        this.operator = text
+                    }else if('='.indexOf(text) >= 0){
+                        this.result = this.removeZero(this.getResult(this.n1, this.n2, this.operator))
+                        this.span.textContent = this.result
+                        this.n1 = ''
+                        this.n2 = ''
+                        this.operator = ''                        
+                    }else if(text === 'Clear'){
+                        this.n1 = ''
+                        this.n2 = ''
+                        this.operator = ''
+                        this.result = ''
+                        this.span.textContent = '0'
+                    }
+                }
+            })
+        }
+        removeZero(string){
+            return string.replace(/.0+$/g, '').replace(/\.0+e/, 'e')
+        }
+        getResult(n1: string, n2: string, operator: string): string{
+            let numberN1: number = parseFloat(n1)
+            let numberN2: number = parseFloat(n2)
+            if(operator === '+'){
+                return (numberN1+numberN2).toPrecision(12)
+            }else if(operator === '-'){
+                return (numberN1-numberN2).toPrecision(12)
+            }else if(operator === '✖️'){
+                return (numberN1*numberN2).toPrecision(12)
+            }else if(operator === '➗'){
+                if(numberN2 === 0){
+                    return '不是数字'
+                }else{
+                    return (numberN1 / numberN2).toPrecision(12)
+                }
+            }
+        }
+        createButton(text: string | number, container: HTMLElement, className?: string){
+            let button = document.createElement('button')
+            button.textContent = text.toString()
+            className && button.classList.add(className)
+            container.appendChild(button)
+        }
     }
-
-    let container = document.createElement('div')
-    container.classList.add('calculator')
-    document.body.appendChild(container)
-
-    let keys: Array<Array<string>> = [
-        ['Clear','➗'],
-        ['7','8','9','✖️'],
-        ['4','5','6','-'],
-        ['1','2','3','+'],
-        ['0','.','='],
-    ];
-
-
-    keys.forEach((textList: Array<string>)=>{
-        let div: HTMLElement = document.createElement('div');
-        textList.forEach((text: string)=>{
-            createButton(text, div);
-        })
-        container.appendChild(div)
-    })
-    
+    new Calculator()
 }
